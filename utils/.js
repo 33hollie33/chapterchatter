39c5995fb36c194db97428f52ec0f4bdcbb46250
33hollie33 <hollybarnhurst@gmail.com>
2023-07-13 21:32:33 +0100

    clean code/debugged

--- utils/.js
+++ utils/.js
@@ -1,33 +1,62 @@
-function fetchDataFromAPI() {
-    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
-        searchQuery
-      )}&key=${apiKey}`;
+// /function fetchDataFromAPI() {
+//     const searchQuery = "Harry Potter";
+//     const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+//         searchQuery
+//       )}&key=${apiKey}`;
 
-    const apiKey = "AIzaSyBxj-ZkK9pnEnACEAfsr_YNYkeIa4IPt1k";
-    const axios = require ("axios")
-    const searchQuery = "Harry Potter";
+//     const apiKey = "AIzaSyBxj-ZkK9pnEnACEAfsr_YNYkeIa4IPt1k";
+//     // const axios = require ("axios")
+    
   
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(bookData => {
+//     fetch(apiUrl)
+//       .then(response => response.json())
+//       .then(bookData => {
 
-        for (var i = 0; i <bookData.length; i++) {
-            var element = document.createElement("p");
-            element.textContent = bookData[i].name;
+//         for (var i = 0; i <bookData.length; i++) {
+//             var element = document.createElement("p");
+//             element.textContent = bookData[i].name;
 
-            var container = document.getElementById("container");
-            container.appendChild(element);
-        }
-        axios
-        .get(url)
-        .then((response) => {
-          console.log(response.data);
-        })
+//             var container = document.getElementById("container");
+//             container.appendChild(element);
+//         }
+//         axios
+//         .get(url)
+//         .then((response) => {
+//           console.log(response.data);
+//         })
 
-        console.log(data);
-      })
-      .catch(error => {
+//         console.log(data);
+//       })
+//       .catch(error => {
         
-        console.error("Error:", error);
-      });
-  }
+//         console.error("Error:", error);
+//       }); 
+//     function fetch(fetchDataFromAPI){
+//         console.log(fetchDataFromAPI);
+
+//     }
+// }
+
+
+function onClickSearch() {
+  const searchQuery = "Harry Potter";
+  const apiKey = "AIzaSyBxj-ZkK9pnEnACEAfsr_YNYkeIa4IPt1k";
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+    searchQuery
+  )}&key=${apiKey}`;
+
+  console.log(apiUrl);
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((bookData) => {
+      const books = bookData.items;
+
+      for (const book of books) {
+        console.log(book.volumeInfo.title);
+      }
+    });
+}
+
+const searchButton = document.getElementById("search");
+searchButton.addEventListener("click", onClickSearch);
