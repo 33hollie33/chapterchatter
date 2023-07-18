37dcf338054fd32d84e9770e85efc8183e39b5c0
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-07-18 19:58:20 +0100

    repo

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,7 @@ router.get('/', async (req, res) => {
   }
 });
 
+
 router.get('/project/:id', async (req, res) => {
   try {
     const projectData = await Project.findByPk(req.params.id, {
@@ -77,6 +78,24 @@ router.get('/login', (req, res) => {
   }
 
   res.render('login');
+});
+
+router.get('/books', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('books', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
